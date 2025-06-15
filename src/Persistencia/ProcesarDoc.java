a441c42bac83b75ec65eeb7099ed115ffda4ea45
Juliana <juliana.defaur2401@alumnos.ubiobio.cl>
2025-06-14 22:39:38 -0400

    mejorado

--- src/Persistencia/ProcesarDoc.java
+++ src/Persistencia/ProcesarDoc.java
@@ -6,33 +6,33 @@ import java.io.FileReader;
 import modelo.*;
 
 public class ProcesarDoc {
-    private AVL arbol; // Árbol donde se insertarán las palabras o frases
+    private AVL arbol; // arbol donde se insertarán las palabras o frases
 
     public ProcesarDoc(AVL arbol) {
         this.arbol = arbol;
     }
 
     public void procesarDoc(String rutaArchivo) {
-        int pagina = 1; // Contador de las páginas
+        int pagina = 1; // contador de las páginas
 
-        try (BufferedReader doc = new BufferedReader(new FileReader(rutaArchivo))) {// El archivo se abrirá para leerlo línea a línea
+        try (BufferedReader doc = new BufferedReader(new FileReader(rutaArchivo))) {// el archivo se abrirá para leerlo línea a línea
 
-            String linea;          // Línea actual del archivo
-            boolean leyendo = false; // Indica si estamos leyendo entre los caracteres "\"
-            String palabra = "";     // Acumulador de la palabra o frase que leemos
+            String linea;
+            boolean leyendo = false; // indica si estamos leyendo entre los caracteres "\"
+            String palabra = "";     // acumulador de la palabra o frase que leemos
 
             while ((linea = doc.readLine()) != null) {
                 for (int i = 0; i < linea.length(); i++) {
-                    char caracter = linea.charAt(i); // Carácter actual de la línea
+                    char caracter = linea.charAt(i); // carácter actual de la línea
 
-                    if (caracter == '|') { // Si encuentra "|", aumenta el número de página
+                    if (caracter == '|') { // si encuentra "|", aumenta el número de página
                         pagina++;
                     } else if (caracter == '\\') {
-                        if (leyendo) { // Si ya estábamos leyendo una palabra, ya que este "\" la cierra
+                        if (leyendo) { // si ya estábamos leyendo una palabra, ya que este "\" la cierra
                             String p = palabra.trim().toLowerCase(); // Limpia y pasa a minúsculas
 
                             if (!p.isEmpty() && p.length() <= 20) {
-                                arbol.insertar(p, pagina); // Inserta en el árbol con la página actual
+                                arbol.insertar(p, pagina); // inserta en el árbol con la página actual
                             }
                             palabra = ""; // limpia la palabra o frase para pasar a la siguiente palabra o frase
                         }
